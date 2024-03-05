2d59233fbd95389b15f6479cd538647ad5aaea60
Klw_m <97058978+Clwmm@users.noreply.github.com>
2024-03-05 16:22:37 +0100

    z1

--- Zestaw01/z5.cpp
+++ Zestaw01/z5.cpp
@@ -3,31 +3,42 @@
 #include <iostream>
 #include <stack>
 
-template<typename T>
-struct StackOperations {
+template<typename S> typename S::value_type sum(S s) {
+    typename S::value_type total; 
+    while(!s.is_empty())
+        total+=s.pop();
+  return total;
+}
+
+template<typename T = int , size_t N = 100> 
+class Stack 
+{
+private:	
+    T _rep[N];
+    size_t _top;
+public:
     typedef T value_type;
+    Stack():_top(0) {};
 
-    static T sum(const std::stack<T>& s) {
-        T total = T();
-        std::stack<T> tempStack = s;
-
-        while (!tempStack.empty()) {
-            total += tempStack.top();
-            tempStack.pop();
-        }
-
-        return total;
+    void push(T val) 
+    {
+        _rep[_top++]=val;
     }
+    T pop()
+    {
+        return _rep[--_top];
+    }
+    bool is_empty() const     
+    {
+        return (_top==0);
+    } 
 };
 
-int main() {
-    std::stack<std::string> stack;
-    stack.push("d");
-    stack.push("c");
-    stack.push("b");
-    stack.push("a");
+auto main() -> int 
+{
+    Stack<double,10> sx;
+    sx.push(3.14);
+    sx.push(2.71);
 
-    std::cout << "Sum of elements on the stack: " << StackOperations<std::string>::sum(stack) << std::endl;
-
-    return 0;
+    std::cout << sum(sx) << std::endl;
 }
